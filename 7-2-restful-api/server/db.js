bf9c203b24c934efb7e14e3105e5182a4b002227
Fajer <s202280160@kfupm.edu.sa>
2025-11-19 11:37:21 +0300

    todo1

--- 7-2-restful-api/server/db.js
+++ 7-2-restful-api/server/db.js
@@ -8,6 +8,11 @@
 import mongoose from "mongoose";
 
 export async function connectDB(url) {
+    try{
   await mongoose.connect(url);
   console.log("[DB] Mongo connected");
+} catch (err){
+        console.error("Connection error:", err.message);
+        throw err;
+    }
 }
